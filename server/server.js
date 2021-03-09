285c28399b468cccdeaffdda657a2a4bab4710cb
thestinx <thestinx@gmail.com>
2021-03-09 13:38:01 -0800

    testing 1

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ const bodyParser = require('body-parser')
 const mongoSchema = require('./schemas/metricsModel.js')
 const app = express();
 const url = "mongodb+srv://KubraKai:codesmith@cluster0.btqz2.mongodb.net/kubrakai?retryWrites=true&w=majority";
+const bcrypt = require('bcrypt')
 
+app.set('view-engine','ejs')
 const connect = mongoose.connect(url, { useNewUrlParser: true });
 connect.then((db) => {
       console.log('Connected correctly to server!');
@@ -18,15 +20,42 @@ connect.then((db) => {
 });
 app.use(bodyParser.json());
 app.use('*', cors());
-
+const fakeDbContent = []
 //Jordan added this
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/build', express.static(path.resolve(__dirname, '../build')));
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../index.html"));
+  res.render('index.ejs',{name:'user'})
+  // res.sendFile(path.resolve(__dirname, "../index.html"));
 });
+app.get('/login',(req,res)=>{
+  res.render('login.ejs')
+})
+app.post('/login',(req,res)=>{
+ 
+})
+app.get('/register',(req,res)=>{
+  res.render('register.ejs')
+})
+app.post('/register', async (req,res)=>{
+ try{
+ const hashPassword = await bcrypt.hash(req.body.password, 10)
+ fakeDbContent.push({
+   id: Date.now().toString(),
+   name:req.body.name,
+   email: req.body.email,
+   password:hashPassword
+ })
+ console.log('im about to redirect')
+ res.redirect('/login') 
+ } catch {
+   console.log('catch')
+res.redirect('/register')
+ } 
+ console.log(fakeDbContent)
+})
 
 const server = new ApolloServer({ 
   introspection: true, 
